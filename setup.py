f45fd731916d659aaa78c8fa3105822b7bfbdae5
Vadym Hutei <hutei@live.com>
2020-03-04 19:07:48 +0200

    [SETUP]

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='egg-test',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Test egg',
+    author='Vadym Hutei',
+    author_email='hutei@live.com'
+)
